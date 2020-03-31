eb17aea2b748047601bdc0d02e376d712336d756
GFwer <GFwer@users.noreply.github.com>
2020-03-31 06:26:09 +0000

    Automated deployment: Tue Mar 31 06:26:09 UTC 2020 GFwer/fawen-emoji@643a74687b28cc6d7b7373bdf00e3b49b4c8a0a6

---
+++ precache-manifest.05796ca1a8ba2da3d75544c3050bd054.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da97bfcb2cc1d515f9b",
+    "url": "/fawen-emoji/css/app.ff718ac5.css"
+  },
+  {
+    "revision": "8c3f46f1f8e0f50a7917",
+    "url": "/fawen-emoji/css/chunk-vendors.4a664f51.css"
+  },
+  {
+    "revision": "5b9301a93e6ea2465e1c5107897e344b",
+    "url": "/fawen-emoji/img/logo.5b9301a9.png"
+  },
+  {
+    "revision": "5874a202b2f1f297d051c0b6abcc8a43",
+    "url": "/fawen-emoji/index.html"
+  },
+  {
+    "revision": "c629da8e759d5fd33325",
+    "url": "/fawen-emoji/js/about.7c09dfdc.js"
+  },
+  {
+    "revision": "1da97bfcb2cc1d515f9b",
+    "url": "/fawen-emoji/js/app.1bc4177a.js"
+  },
+  {
+    "revision": "8c3f46f1f8e0f50a7917",
+    "url": "/fawen-emoji/js/chunk-vendors.bf351920.js"
+  },
+  {
+    "revision": "907f3b439118121134610fcd77d1049d",
+    "url": "/fawen-emoji/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fawen-emoji/robots.txt"
+  }
+]);
