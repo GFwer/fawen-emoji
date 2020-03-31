3a2343d8a71d68c10da0ab2229a3fe5224b32acd
Fawen <sngfw123@gmail.com>
2020-03-31 17:14:02 +0800

    :wrench: add workflow

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # fawen-emoji
 
+![github pages](https://github.com/GFwer/fawen-emoji/workflows/github%20pages/badge.svg?branch=master&event=push)
+
+
 emoji tool for git commit message
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fawen-emoji/'
+    : '/',
+  outputDir: './docs',
   transpileDependencies: [
     'vuetify',
   ],
+
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  }
 };
